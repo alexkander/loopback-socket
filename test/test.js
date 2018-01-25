9a6a71dcf291d0e518d8da711db51f3029ea0713
Alex J. Rondón <arondon@sirideas.com>
2018-01-24 23:44:15 -0500

    resolve fix in testing

--- test/test.js
+++ test/test.js
@@ -4,8 +4,8 @@ const expect = require('chai').expect;
 const Module = require('../index');
 
 describe('#Module', () => {
-  it('should be null', () => {
-    var result = Module();
-    expect(result).to.equal(null);
+  it('should be itself', () => {
+    var result = new Module();
+    expect(result).to.equal(result);
   });
 });
