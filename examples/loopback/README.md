e71c804bc062a2bd2b08ccc5471c4369e8362f0e
Alex J. Rondón <arondon@sirideas.com>
2018-01-29 20:05:58 -0500

    sample updated

--- examples/loopback/README.md
+++ examples/loopback/README.md
@@ -1,4 +1,4 @@
-# LoopbackSocket Sample
+# Loopback + AngularJS Sample
 
 ## Installation
 
@@ -12,3 +12,134 @@
  ```
  node .
  ```
+
+## Relevants codes
+
+### Server side
+
+#### Starting
+
+```js
+// server/server.js
+// ....
+const lbSocket = LoopbackSocket.get('myapp');
+const socketServer = io(handler);
+lbSocket.start(socketServer);
+// ....
+```
+
+#### Authentication
+
+```js
+// server/models/my-user.js
+// ....
+const lbSocket = LoopbackSocket.get('myapp');
+
+lbSocket.auth({ model: MyUser, method: 'authenticate', });
+MyUser.authenticate = function (socket, credentials) {
+  return Promise.resolve()
+  .then(() => {
+    if (!credentials) throw 'Socket Credentials not recieved';
+    return app.models.AccessToken.findOne({
+      where: {
+        userId: credentials.userId,
+        id: credentials.id
+      }
+    });
+  })
+  .then((token) => {
+    if (!token) throw 'Socket Token not found';
+    return MyUser.findById(token.userId);
+  })
+  .then((user) => {
+    if (!user) throw 'Socket User not found';
+  })
+};
+// ....
+```
+
+#### Methods definitions
+
+```js
+// server/models/my-user.js
+// ....
+lbSocket.defineMethod('getProfile', { model: MyUser, method: 'getProfile', });
+MyUser.getProfile = function (socket, credentials, args) {
+  return app.models.MyUser.findById(credentials.userId);
+};
+// ....
+
+// server/models/my-model.js
+// ....
+const lbSocket = LoopbackSocket.get('myapp');
+lbSocket.defineMethod('getRecords', { model: MyModel, method: 'getRecords', });
+MyModel.getRecords = function (socket, credentials, args) {
+  return MyModel.find({
+    skip: args.skip,
+    limit: args.limit,
+  });
+};
+// ....
+```
+
+### Client side
+
+#### AngularJS Digest creator callback
+```
+.service('Handler', function () {
+  return ($scope, cb) => {
+    return function () {
+      const args = arguments;
+      $scope.$apply(() => {
+        cb.apply(null, args);
+      });
+    };
+  };
+})
+```
+
+#### Sockect events handlers
+```js
+.service('CustomSocket', function (Handler) {
+  return ($scope, url, token) => {
+    const socket = io.connect(url);
+    socket.authed = null;
+
+    socket.on('connect', Handler($scope, () => {
+      socket.emit('authentication', {
+        id: token.id,
+        userId: token.userId,
+      });
+    }));
+
+    socket.on('authenticated', Handler($scope, () => {
+      socket.authed = true;
+    }));
+
+    socket.on('unauthorized', Handler($scope, () => {
+      socket.authed = false;
+    }));
+
+    socket.on('disconnected', Handler($scope, () => {
+      socket.authed = null;
+    }));
+
+    return socket;
+
+  }
+})
+```
+
+#### Login and socket connection
+
+```js
+$scope.promiseLogin = MyUser.login($scope.credentials).$promise
+.then((response) => {
+  token = response;
+  $scope.socket = socket = CustomSocket($scope, 'http://localhost:3000', token);
+  socket.authed = null;
+})
+.finally(() => {
+  delete $scope.promiseLogin;
+});
+```
--- examples/loopback/client/static/app.js
+++ examples/loopback/client/static/app.js
@@ -4,93 +4,48 @@ angular.module('app', [
   'ngResource'
 ])
 
-.config(['$httpProvider', ($httpProvider) => {
-  $httpProvider.interceptors.push('MyInterceptor');
-}])
-
-.factory('MyInterceptor', ($q, $rootScope) => { 'ngInject';
-  return {
-    'request': (config) => {
-      console.log('MyUser.$token', $rootScope.$token)
-      if (!$rootScope.$token) return config;
-
-      if ($rootScope.$token.id) {
-        config.headers.authorization = $rootScope.$token.id;
-      } else if (config.__isGetCurrentUser__) {
-        // Return a stub 401 error for User.getCurrent() when
-        // there is no user logged in
-        const res = {
-          body: { error: { status: 401 }},
-          status: 401,
-          config: config,
-          headers: () => undefined,
-        };
-        return $q.reject(res);
-      }
-      return config || $q.when(config);
-    },
-  };
-})
-
-.service('MyUser', function ($rootScope, $resource) {
+.service('MyUser', function ($resource) {
   const MyUser = $resource('/api/MyUsers/:id', { 'id': '@id' }, {
     login: {
       method: 'post',
       url: '/api/MyUsers/login'
     }
   });
-  const loginMethod = MyUser.login;
-  MyUser.login = function () {
-    const response = loginMethod.apply(MyUser, arguments)
-
-    response.$promise
-    .then((result) => {
-      $rootScope.$token = result;
-    });
-
-    return response;
-  };
-
-  MyUser.getCurrentToken = function () {
-    return $rootScope.$token;
-  };
   return MyUser;
 })
 
-.service('Utils', function () {
-  return {
-    handler: ($scope, cb) => {
-      return function (aaa) {
-        const args = arguments;
-        $scope.$apply(() => {
-          cb.apply(null, args);
-        });
-      };
-    }
+.service('Handler', function () {
+  return ($scope, cb) => {
+    return function () {
+      const args = arguments;
+      $scope.$apply(() => {
+        cb.apply(null, args);
+      });
+    };
   };
 })
 
-.service('CustomSocket', function (Utils) {
+.service('CustomSocket', function (Handler) {
   return ($scope, url, token) => {
     const socket = io.connect(url);
     socket.authed = null;
 
-    socket.on('connect', Utils.handler($scope, () => {
+    socket.on('connect', Handler($scope, () => {
       socket.emit('authentication', {
         id: token.id,
         userId: token.userId,
       });
     }));
 
-    socket.on('authenticated', Utils.handler($scope, () => {
+    socket.on('authenticated', Handler($scope, () => {
       socket.authed = true;
     }));
 
-    socket.on('unauthorized', Utils.handler($scope, () => {
+    socket.on('unauthorized', Handler($scope, () => {
       socket.authed = false;
     }));
 
-    socket.on('disconnected', Utils.handler($scope, () => {
+    socket.on('disconnected', Handler($scope, () => {
       socket.authed = null;
     }));
 
@@ -99,8 +54,10 @@ angular.module('app', [
   }
 })
 
-.controller('MainController', function ($scope, MyUser, CustomSocket, Utils) {
+.controller('MainController', function ($scope, MyUser, CustomSocket, Handler) {
   
+  let socket, token;
+
   $scope.credentials = {
     email: 'arondn2@gmail.com',
     password: '123456',
@@ -108,35 +65,60 @@ angular.module('app', [
 
   $scope.doLogin = function () {
     if ($scope.promiseLogin) return;
+    token = null;
+    if (socket) {
+      socket.emit('disconnect');
+      $scope.socket = socket = null;
+    }
     $scope.promiseLogin = MyUser.login($scope.credentials).$promise
-    .then(() => {
-      $scope.socketConnect();
+    .then((response) => {
+      token = response;
+      $scope.socket = socket = CustomSocket($scope, 'http://localhost:3000', token);
+      socket.authed = null;
     })
     .finally(() => {
       delete $scope.promiseLogin;
     });
   };
 
-  let socket;
-  $scope.socketConnect = function () {
-    $scope.socket = socket = CustomSocket($scope, 'http://localhost:3000', MyUser.getCurrentToken());
-    socket.authed = null;
+  $scope.exec = function (method, args) {
+    if (!socket) {
+      method.response = 'disconnected';
+    } else if(!socket.authed){
+      method.response = 'unauthorized';
+    } else {
+      method(args);
+    }
   };
 
-  $scope.getProfile = () => {
-    socket.emit('getProfile', $scope.getProfile.args, Utils.handler($scope, (response) => {
-      $scope.getProfile.response = response;
-    }));
-  };
-  $scope.getProfile.args = {};
+  $scope.responses = {};
+  $scope.args      = {};
 
-  $scope.getRecords = () => {
-    socket.emit('getRecords', $scope.getRecords.args, Utils.handler($scope, (response) => {
-      $scope.getRecords.response = response;
+  $scope.getProfile = (args) => {
+    $scope.responses.getProfile = null;
+    socket.emit('getProfile', args, Handler($scope, (response) => {
+      $scope.responses.getProfile = response;
     }));
   };
-  $scope.getRecords.args = {};
+  $scope.args.getProfile = {};
+
+  $scope.getRecords = (args) => {
+    $scope.responses.getRecords = null;
+    socket.emit('getRecords', args, Handler($scope, (response) => {
+      $scope.responses.getRecords = response;
+    }));
+  };
+  $scope.args.getRecords = {};
 
   $scope.doLogin();
 
+})
+
+.filter('socketState', function () {
+  return (socket) => {
+    if(!socket) return 'disconnected';
+    if(socket.authed===true) return 'authenticated';
+    if(socket.authed===false) return 'unauthorized';
+    return 'connecting';
+  };
 });
