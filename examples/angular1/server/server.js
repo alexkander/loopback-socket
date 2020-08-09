3881e32b22a9568d80c887c684d43e4be26e8628
Alexander Rondon <arondn2@gmail.com>
2020-08-08 23:35:14 -0500

    fix: remove loopback-component-explorer module used

--- examples/angular1/server/server.js
+++ examples/angular1/server/server.js
@@ -13,10 +13,6 @@ app.start = function() {
     app.emit('started');
     const baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      const explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
 
     // LoobakSocket setup
     const loopbackSocket = LoopbackSocket.get('myapp');
